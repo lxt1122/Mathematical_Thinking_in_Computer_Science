f29afe809419e18a65c131b657fc74cf673110c5
xt <lional1122lee@gmail.com>
2022-06-19 19:28:20 +0800

    20220619

--- Week_2/a_6digit.py
+++ Week_2/a_6digit.py
@@ -4,3 +4,9 @@
 for i in range(100000, 100999):
     if i % 9127 == 0:
         print(i)
+
+
+for j in range(100, 999):
+
+    if j % 2 == 1 and j % 3 == 1 and j % 4 == 1 and j % 5 == 1 and j % 6 == 1 and j % 7 == 1:
+            print(j)
