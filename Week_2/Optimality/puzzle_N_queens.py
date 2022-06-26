9e6b17f4fabbef47054263dc81e48d19db995a56
xt <lional1122lee@gmail.com>
2022-06-26 17:41:00 +0800

    20220626

--- Week_2/Optimality/puzzle_N_queens.py
+++ Week_2/Optimality/puzzle_N_queens.py
@@ -3,7 +3,7 @@
 # Place 8 queens such that no two attack each other.
 
 #  Is it possible to place n queens on an n*n chessboard such that no 2 queens attack each other?
-#  1.single Q in each row/column/diagonal
+#  1.single Q in each row/column/diagonal.
 
 
 import itertools as it
